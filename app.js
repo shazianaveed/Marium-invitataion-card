8203f2f4b79e02c49c1fe4ff3d8f8ee06e77a4ab
shazianaveed <160173580+shazianaveed@users.noreply.github.com>
2024-10-28 21:57:48 +0500

    added html-css-js

---
+++ app.js
@@ -0,0 +1,3 @@
+document.getElementById('rsvp-button').addEventListener('click', function() {
+    alert('Mr & Mrs Rashid Ahmed Siddiqui! \n  0345-8299213 \n Afnan Ahmed Siddiqui \n 0317-4481019 \n Wamiq Ahmed Siddiqui \n 0317-0002101');
+});
